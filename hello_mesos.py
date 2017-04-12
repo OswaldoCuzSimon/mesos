b5f85dfff6acd2a0f2df86f11378b7bdef122e9d
oswaldocruzsimon <oswaldo_cruz@javeriana.edu.co>
2017-04-12 16:40:23 -0500

    se hace en el print con echo en stderr

--- hello_mesos.py
+++ hello_mesos.py
@@ -2,4 +2,4 @@ import sys
 
 for i in range(1,10):
 	print("echo \""+str(i)+" hello mesos\"")
-	sys.stderr.write("echo \""+str(i)+" hello mesos\"")
+	sys.stderr.write("echo \""+str(i)+" hello mesos\"\n")
