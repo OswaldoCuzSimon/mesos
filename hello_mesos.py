f34abafeb998a75febe3915900070e71575b96f2
oswaldocruzsimon <oswaldo_cruz@javeriana.edu.co>
2017-04-12 17:41:01 -0500

    se agregaron json para exec jobs

--- hello_mesos.py
+++ hello_mesos.py
@@ -1,5 +1,7 @@
-import sys
-
-for i in range(1,10):
+import sys,time
+i=1
+while True:
 	print("echo \""+str(i)+" hello mesos\"")
 	sys.stderr.write("echo \""+str(i)+" hello mesos\"\n")
+	i=i+1
+	time.sleep(1)
