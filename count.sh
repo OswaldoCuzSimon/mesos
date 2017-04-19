177d39bc5e6a3c6e15c98b7cf19421e91c84a5ac
oswaldocuzsimon <oswaldo_cs_94@hotmail.com>
2017-04-19 13:03:25 -0500

    se cambio el ciclo con while

--- count.sh
+++ count.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-for i in `seq 1 1000000000`;
-do
-	echo $i
-done
+ #!/bin/bash 
+         COUNTER=0
+         while [  $COUNTER -lt 1000000 ]; do
+             echo $COUNTER
+             let COUNTER=COUNTER+1 
+         done
 #bash count.sh  6.57s user 3.02s system 45% cpu 21.077 total
