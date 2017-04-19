58c03f0579150781d4529eabf603b7b760d37ca7
oswaldocruzsimon <oswaldo_cruz@javeriana.edu.co>
2017-04-19 12:24:12 -0500

    se actualizo el hello_mesos

--- hello_mesos.py
+++ hello_mesos.py
@@ -1,5 +1,3 @@
 import sys,time
-for i in range(1,21):
-	print("echo \""+str(i)+" hello mesos\"")
-	sys.stderr.write("echo \""+str(i)+" hello mesos\"\n")
-	time.sleep(1)
+for i in range(0,1000000):
+	print(i)
