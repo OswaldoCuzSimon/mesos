5c4cd26b5b45b27d5dbb7092ae304ad70f650f15
oswaldocuzsimon <oswaldo_cs_94@hotmail.com>
2017-04-21 12:23:07 -0500

    se cambio el num de iteraciones

--- count.sh
+++ count.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
  #!/bin/bash 
-         COUNTER=0
-         while [  $COUNTER -lt 1000000 ]; do
+COUNTER=0
+while [  $COUNTER -lt 1000000 ]; do
              echo $COUNTER
              let COUNTER=COUNTER+1 
-         done
+done
 #bash count.sh  6.57s user 3.02s system 45% cpu 21.077 total
--- hello_mesos.py
+++ hello_mesos.py
@@ -1,4 +1,4 @@
 import sys,time
-for i in range(0,10000000):
+for i in range(0,1000000):
 	print(i)
 #1.04s user 2.39s system 17% cpu 19.728 total
